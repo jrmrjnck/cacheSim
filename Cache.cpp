ae60dd1d635b3ed8b70bee53cd74e2847940d403
Jonathan Doman <jonathan.doman@gmail.com>
2010-10-11 10:31:08 -0400

    Implemented Cache class as a QAbstractTableModel
     - set internal data structures
     - defined pure virtual function from base class

--- Cache.cpp
+++ Cache.cpp
@@ -7,5 +7,76 @@
  *
 \****************************************************************************/
 
+#include <QtCore/QVariant>
+
 #include "Cache.h"
 
+Cache::Cache( int wordsPerBlock, int lines ) : QAbstractTableModel(),
+                                               _cache(lines,wordsPerBlock)
+{
+   _cacheLines = lines;
+   _wordsPerBlock = wordsPerBlock;
+
+   for( int i = 0; i < lines; ++i ) {
+      for( int j = 0; j < wordsPerBlock; ++j ) {
+         _cache[i].words[j] = 0;
+      }
+   }
+}
+
+int Cache::rowCount( const QModelIndex& parent ) const
+{
+   if( parent.isValid() )
+      return 0;
+
+   return _cache.size();
+}
+
+int Cache::columnCount( const QModelIndex& parent ) const
+{
+   if( parent.isValid() )
+      return 0;
+
+   return (_wordsPerBlock + 2);
+}
+
+QVariant Cache::data( const QModelIndex& index, int role ) const
+{
+   // Check validity of request
+   if( !index.isValid() || role != Qt::DisplayRole )
+      return QVariant();
+
+   int line = index.row();
+   int column = index.column();
+
+   // Dirty bit
+   if( column == 0 ) {
+      return (_cache[line].dirty ? QVariant("1") : QVariant("0"));
+   }
+   else if( column == 1 ) {
+      int tag = _cache[line].tag;
+      return QVariant(QString::number(tag,16));
+   }
+   else {
+      return QVariant(QString::number(_cache[line].words[column-2],16));
+   }
+}
+
+QVariant Cache::headerData( int section, Qt::Orientation orientation, int role ) const
+{
+   if( role != Qt::DisplayRole )
+      return QVariant();
+
+   if( orientation == Qt::Vertical ) {
+      return QVariant( "0x" + QString::number(section,16) );
+   }
+
+   if( section == 0 )
+      return QVariant( "Dirty" );
+   else if( section == 1 )
+      return QVariant( "Tag" );
+   else
+      return QVariant( section - 2 );
+
+   return QVariant();
+}
--- Cache.h
+++ Cache.h
@@ -6,10 +6,47 @@
  * October 17, 2010
  *
 \****************************************************************************/
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <QAbstractTableModel>
+#include <QtCore/QVector>
+
+class QModelIndex;
+class CacheLine;
 
 // A model of a direct-mapped cache
-class Cache
+class Cache : public QAbstractTableModel
 {
+   Q_OBJECT
+ 
+public:
+   Cache( int wordsPerBlock, int lines );
+
+public:
+   int rowCount( const QModelIndex& parent = QModelIndex() ) const;
+   int columnCount( const QModelIndex& parent = QModelIndex() ) const;
+   QVariant data( const QModelIndex& index, int role = Qt::DisplayRole ) const;
+   QVariant headerData( int section, Qt::Orientation o, int role = Qt::DisplayRole ) const;
    
+private:
+   QVector<CacheLine> _cache;
+
+   int _cacheLines;
+   int _wordsPerBlock;
 };
 
+class CacheLine
+{
+public:
+   CacheLine( int numWords = 0 ) : dirty(0),
+                                   tag(0),
+                                   words(new int[numWords])
+   {};
+
+   bool dirty;
+   int  tag;
+   int* words;
+};
+
+#endif // CACHE_H
