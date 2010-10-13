dd3336be629b197d98d0d778feccf7dca7964a58
Jonathan Doman <jonathan.doman@gmail.com>
2010-10-13 13:14:28 -0400

    Add Processor class
    implements a view and a model for the running programs

---
+++ Processor.h
@@ -0,0 +1,26 @@
+/****************************************************************************\
+ *
+ * Jonathan Doman
+ * 905260988
+ * ECE 3574 - Homework 4
+ * October 17, 2010
+ *
+\****************************************************************************/
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+#include <QWidget>
+
+class Processor : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Processor(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // PROCESSOR_H
